946745c96fcf92a6555de0ee162f51c7396047e6
mostafa dbagh <dabagmostafa@gmail.com>
2021-04-19 03:18:44 -0700

    hi

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:'https://thawing-harbor-47972.herokuapp.com/api'
+    // baseURL:'https://thawing-harbor-47972.herokuapp.com/api'
+    baseURL:'http://localhost:30001/api'
 })
 
 export const insertUser = payloadd => api.post('/Login',payloadd)
