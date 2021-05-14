2fa0c5578c061a199b1292226a208c8a3b600dc3
mostafa dbagh <dabagmostafa@gmail.com>
2021-05-13 17:34:07 -0700

    hello

--- client/src/components/main-image-slider.js
+++ client/src/components/main-image-slider.js
@@ -3,6 +3,7 @@
 import {MainDiv,Overlay,H1,P,Image,Image2} from '../style/mainstyle'
 import React  from 'react'
 import resp from '../img/resp.jpg'
+import resp2 from '../img/resp3.jpg'
 import Firts from '../img/anwar.jpg'
 import Second from '../img/second.jpg'
 import {useMediaQuery} from 'react-responsive'
@@ -10,22 +11,24 @@ import { useState } from 'react'
 const Main  = () => {
   const [change,setChange] = useState(false);
   
-  const safiquery = useMediaQuery({
-    query:'(max-width:470px)'
+  const query = useMediaQuery({
+    query:'(max-width:510px)'
 })
   return (  
 
      <MainDiv >
-       {safiquery ?
+       {query ?
        <>
-          <img src={ Second} width="100%" height="100%" style={{objectFit:'container',position:'absolute',top:'0',left:'0'}}/> 
-          <Image src={ resp} width="100%" height="100%" style={{objectFit:'container',position:'absolute',top:'0',left:'0'}}/>
+          <img src={resp2} width="100%" height="100%" style={{objectFit:'container',position:'absolute',top:'0',left:'0'}}/> 
+          <Image src={resp2} width="100%" height="100%" style={{objectFit:'container',position:'absolute',top:'0',left:'0'}}/>
           </>
+          : <>
+          <img src={ Second} width="100%" height="100%" style={{objectFit:'container',position:'absolute',top:'0',left:'0'}}/> 
+         <Image src={ Firts} width="100%" height="100%" style={{objectFit:'container',position:'absolute',top:'0',left:'0'}}/> 
+        </>
 
-      : <> 
-       <img src={ Second} width="100%" height="100%" style={{objectFit:'container',position:'absolute',top:'0',left:'0'}}/> 
-      <Image src={ Firts} width="100%" height="100%" style={{objectFit:'container',position:'absolute',top:'0',left:'0'}}/> 
-     </>}
+       }
+      
  </MainDiv> 
 
    
