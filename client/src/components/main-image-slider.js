fe53966524441af52478fd0c236085c2327101b5
mostafa dbagh <dabagmostafa@gmail.com>
2021-05-13 17:48:25 -0700

    helo

--- client/src/components/main-image-slider.js
+++ client/src/components/main-image-slider.js
@@ -20,7 +20,7 @@ const Main  = () => {
        {query ?
        <>
           <img src={resp2} width="100%" height="100%" style={{objectFit:'container',position:'absolute',top:'0',left:'0'}}/> 
-          <Image src={resp1} width="100%" height="100%" style={{objectFit:'container',position:'absolute',top:'0',left:'0'}}/>
+          <Image src={resp} width="100%" height="100%" style={{objectFit:'container',position:'absolute',top:'0',left:'0'}}/>
           </>
           : <>
           <img src={ Second} width="100%" height="100%" style={{objectFit:'container',position:'absolute',top:'0',left:'0'}}/> 
